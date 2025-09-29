3cf059a5a4fcbd6d208c42e63bce0d8a43bbe2f8
Muhammad Saddam Nur <muhammadsaddamnur@gmail.com>
2025-09-29 16:43:28 +0700

    feat: add sentry error monitoring and env configuration
    - Add sentry SDK for error tracking
    - Configure environment variables using flutter_dotenv
    - Update pubspec dependencies and asset configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,32 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:store/core/di/injection_container.dart' as di;
+import 'package:sentry/sentry.dart';
 import 'package:store/features/splashscreen/presentation/pages/splashscreen_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
 
-  /// Initialize the dependency injection container
-  await di.init();
+  runZonedGuarded(
+    () async {
+      print(dotenv.env['SENTRY_DSN']);
+      await Sentry.init((options) {
+        options.dsn = dotenv.env['SENTRY_DSN'] ?? '';
+      });
 
-  runApp(const MyApp());
+      /// Initialize the dependency injection container
+      await di.init();
+
+      // Init your App.
+      runApp(const MyApp());
+    },
+    (exception, stackTrace) async {
+      await Sentry.captureException(exception, stackTrace: stackTrace);
+    },
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -18,9 +36,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
       home: const SplashScreenPage(),
     );
   }
